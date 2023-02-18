3687248f6ade55d66642fb1f9c0a47a742816d80
Георгий howl <77685644+HyperHowl@users.noreply.github.com>
2023-02-18 22:45:27 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from typing import (
-    Any, Dict, Generic, Optional,
-    TypeVar, Union, get_origin
+    Any, Dict, Generic, Optional, TypeVar, Union, get_origin
 )
 
 from pydantic import BaseModel as OriginalBaseModel
 from pydantic import root_validator
 from pydantic.json import ENCODERS_BY_TYPE
 
-T = TypeVar("T")
-V = TypeVar("V")
+_T = TypeVar("T")
+_V = TypeVar("V")
 
 
-class Result(Generic[T]):
+class Result(Generic[_T]):
     """Result type in order to correctly handle None or Any value."""
-    __value: Optional[T]
+    __value: Optional[_T]
     
-    def __init__(self, value: Optional[T] = None) -> None:
+    def __init__(self, value: Optional[_T] = None) -> None:
         self.__value = value
 
     def __str__(self) -> str:
@@ -37,7 +36,7 @@ class Result(Generic[T]):
         """Return True if Result value is None."""
         return self.__value is None
       
-    def unwrap(self, *, error_msg: str = None) -> T:
+    def unwrap(self, *, error_msg: str = None) -> _T:
         """Unwrap the Result value if it's not None, otherwise a
         ValueError will be raised with the appropriate error_msg.
         """
@@ -45,7 +44,7 @@ class Result(Generic[T]):
             raise ValueError(error_msg or "Value is None.")
         return self.__value
     
-    def unwrap_or(self, variant: V, /) -> Union[T, V]:
+    def unwrap_or(self, variant: _V, /) -> Union[_T, _V]:
         """Unwrap the Result value if it's not None,
         otherwise it will return the variant you passed.
         """
@@ -61,30 +60,22 @@ class BaseModel(OriginalBaseModel):
         model_schema = cls.schema()
         fields = _get_defaults(model_schema)
         fields.update(
-            _get_required_result_fields(
-                cls.__annotations__, model_schema
-            )
+            _get_required_result_fields(cls.__annotations__, model_schema)
         )
         fields.update(values)
         return {
-            field: Result(value) if _is_result_type(
-                cls.__annotations__.get(field, None)
-            ) and not isinstance(value, Result)
-            else value for field, value in fields.items()
+            field: Result(value) if _is_result_type(cls.__annotations__.get(field, None))
+            and not isinstance(value, Result) else value for field, value in fields.items()
         }
     
 
 def _is_result_type(type_: type) -> bool:
     origin_type = get_origin(type_)
-    
-    if origin_type is Union:
-        origin_type = type_
-    else:
-        origin_type = (
-            type(type_) if type_ is None
-            else origin_type or type_
-        )
-    
+    result_type = (
+        type(type_) if type_ is None else
+        type_ if origin_type is Union else
+        origin_type or type_
+    )
     return issubclass(Result, origin_type)
 
 
@@ -92,27 +83,18 @@ def _get_defaults(schema: Dict[str, Any]) -> Dict[str, Any]:
     properties = schema.get("properties", {})
     if not properties:
         return {}
-    
     return {
-        k: properties[k]["default"]
-        for k in properties
-        if "default" in properties[k]
+        k: properties[k]["default"] for k in properties if "default" in properties[k]
     }
 
 
-def _get_required_result_fields(
-    annotations: Dict[str, Any],
-    schema: Dict[str, Any]
-) -> Dict[str, "Result"]:
+def _get_required_result_fields(annotations: Dict[str, Any], schema: Dict[str, Any]) -> Dict[str, "Result"]:
     properties = schema.get("properties", {})
     if not properties:
         return {}
-
     return {
-        k: Result() for k in properties
-        if k in schema.get("required", [])
-        or "default" not in properties[k]
-        and _is_result_type(annotations[k])
+        k: Result() for k in properties if k in schema.get("required", [])
+        or "default" not in properties[k] and _is_result_type(annotations[k])
     }
 
 
